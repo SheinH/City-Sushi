8d9e370952099419c40c6b61109b22339923bc35
GarlandQ <garqiu@gmail.com>
2019-12-06 02:53:35 -0500

    included a few more models we can maybe use

--- orders/models.py
+++ orders/models.py
@@ -6,8 +6,29 @@ from django.db import models
 from django.db import models
 
 
-class User(models.Model):
-    pass
+#class User(models.Model):
+    #pass
+
+# Added a few more models. Feel free to change this.
+# I'll leave the pkey and fkey out for now.
+class User(AbstractUser):
+    is_customer = models.BooleanField(default=False)
+
+class Customer(models.Model):
+    f_name = models.CharField(max_length=20,blank=False)
+    l_name = models.CharField(max_length=20,blank=False)
+    phone = models.CharField(max_length=10,blank=False)
+    address = models.TextField()
+
+class Restaurant(models.Model):
+    r_name = models.CharField(max_length=100,blank=False)
+    info = models.CharField(max_length=100,blank=False)
+    location = models.CharField(max_length=50,blank=False)
+
+class Menu(models.Model):
+    item_id = models.ForeignKey(Item,on_delete=models.CASCADE)
+    price = models.IntegerField(blank=False)
+    quantity = models.IntegerField(blank=False,default=0)
 
 
 # Replace pass with more data
