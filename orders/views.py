d172a53657cfe09b60f3aa0b3cf0774e482a75f1
Shein Htike <chocolateshein.htike@gmail.com>
2019-12-06 11:10:14 -0500

    Added Menu view

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+import django.views.generic
+from django.views import generic
+from .models import Dish
+from django.shortcuts import render
+
+def menu(request):
+    context = {'items' : Dish.objects.all()}
+    print(Dish.objects.all())
+    return render(request, 'orders/menu.html', context)
