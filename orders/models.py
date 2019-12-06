3056e0caa2b023bd1c2b628356427f7fbca55d75
Shein Htike <chocolateshein.htike@gmail.com>
2019-12-05 21:26:37 -0500

    Expanded sample models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,4 @@
+
 from django.core.exceptions import ValidationError
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
@@ -5,9 +6,8 @@ from django.db import models
 # Create your models here.
 from django.db import models
 
+from django.contrib.auth.models import User
 
-class User(models.Model):
-    pass
 
 
 # Replace pass with more data
@@ -16,15 +16,26 @@ class Dish(models.Model):
     name = models.CharField(max_length=60)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
+    def rating(self):
+        reviews = self.review_set.all()
+        scores = [x.rating for x in reviews]
+        return sum(scores) / len(scores)
+
 
 class Review(models.Model):
     dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
-    reviewer = models.ForeignKey(User)
+    reviewer = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     rating = models.IntegerField(
         default=5,
         validators=[MaxValueValidator(5), MinValueValidator(1)]
     )
     review_text = models.TextField(blank=True,null=True)
+
+    def __str__(self):
+        return f'{self.reviewer}: {self.rating} stars'
 
     def clean(self):
         super().clean()
