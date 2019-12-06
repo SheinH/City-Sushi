a9e57be644ce5f01294a95f2218eb38bef5ef74f
Shein Htike <chocolateshein.htike@gmail.com>
2019-12-05 20:31:16 -0500

    created sample models

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,32 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class User(models.Model):
+    pass
+
+
+# Replace pass with more data
+
+class Dish(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.TextField()
+
+
+class Review(models.Model):
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    reviewer = models.ForeignKey(User)
+    rating = models.IntegerField(
+        default=5,
+        validators=[MaxValueValidator(5), MinValueValidator(1)]
+    )
+    review_text = models.TextField(blank=True,null=True)
+
+    def clean(self):
+        super().clean()
+        if self.rating <= 3 and self.rating is None:
+            raise ValidationError('Review required for low rating')
