9ba3d984dd4127ad693731c196cce584b1936ccb
Shein Htike <chocolateshein.htike@gmail.com>
2019-12-06 15:43:49 -0500

    .gitignore is now working

--- orders/admin.py
+++ orders/admin.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
 
 from .models import *
 
-# We will need to tidy up on the .models we imported.
 
-admin.site.register(User)
-admin.site.register(Customer)
-admin.site.register(Restaurant)
-admin.site.register(Menu)
+
 admin.site.register(Dish)
 admin.site.register(Review)
--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,7 @@
-from django.urls import include, path
+from django.urls import path
+
 from . import views
 
-
 urlpatterns = [
-    # path('',views.index,name='index'),
     path('menu', views.menu, name='menu'),
-    path('orderplaced/', views.orderplaced),
 ]
