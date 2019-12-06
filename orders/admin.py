561be6f716bc3247d2fae89fbe4a34ccd95aef13
GarlandQ <garqiu@gmail.com>
2019-12-06 02:52:54 -0500

    added sample registers

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import User, Customer, Restaurant, Menu, Dish, Review
+
+# We will need to tidy up on the .models we imported.
+
+admin.site.register(User)
+admin.site.register(Customer)
+admin.site.register(Restaurant)
+admin.site.register(Menu)
+admin.site.register(Dish)
+admin.site.register(Review)
