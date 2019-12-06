4fe1bf3a36f69a9837862005c02b240ca0212575
GarlandQ <garqiu@gmail.com>
2019-12-06 18:49:05 -0500

    deleted restaurant form since it was not needed

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from .forms import CustomerSignUpForm, CustomerForm, RestuarantForm, ReviewForm
+from .forms import CustomerSignUpForm, CustomerForm, RestaurantForm, ReviewForm
 # Create your views here.
 from django.shortcuts import render
+from .models import Visitor, Customer, Payment_Info, Dish, Review
 
-from .models import Dish
 
 # Homepage
 def index(request):
@@ -12,63 +12,84 @@ def index(request):
 
 
 def menu(request):
-    context = {'items' : Dish.objects.all()}
+    context = {'items': Dish.objects.all()}
     print(Dish.objects.all())
     return render(request, 'orders/menu.html', context)
 
+
 def orderPlaced(request):
-	return render(request, 'orders/orderPlaced.html', context)
+    return render(request, 'orders/orderPlaced.html', {})
+
 
 # logout for both users (restaurants and regular customers)
 def Logout(request):
-	if request.user.is_restaurant:
-		logout(request)
-		return redirect("rlogin")
-	else:
-		logout(request)
-		return redirect("login")
+    if request.user.is_restaurant:
+        logout(request)
+        return redirect("rlogin")
+    else:
+        logout(request)
+        return redirect("login")
+
 
 # Customer side of things
 
 # Register customer account
 def customerRegister(request):
-	form = CustomerSignUpForm(request.POST or None)
-	if form.is_valid():
-		user = form.save(commit = False)
-		username = form.cleaned_data['username']
-		password = form.cleaned_data['password']
-		user.is_customer = True
-		user.set_password(password)
-		user.save()
-		user = authenticate(username = username, password = password)
-		if user is not None:
-			if user.is_active:
-				login(request, user)
-				return rredirect("ccreate")
-	return render(request, 'orders/signup.html', context = {'form': form})
+    form = CustomerSignUpForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user.is_customer = True
+        user.set_password(password)
+        user.save()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect("ccreate")
+    return render(request, 'orders/signup.html', context={'form': form})
 
 
 # Let customers log in
 def customerLogin(request):
-	if request.method == "POST":
-		username = request.post['username']
-		password = request.post['password']
-		user = authenticate(username = username, password = password)
+    if request.method == "POST":
+        username = request.post['username']
+        password = request.post['password']
+        user = authenticate(username=username, password=password)
 
-		if user is not None:
-			if user.is_active:
-				login(request, user)
-				return redirect("profile")
-			else:
-				return render(request, 'orders/login.html', {'error message': 'Invalid Username and Password'})
-		else:
-			return render(request, 'orders/login.html', {'error message': 'Invalid Username and Password'})
-	return render(request, 'orders/login.html')
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect("profile")
+            else:
+                return render(request, 'orders/login.html', {'error message': 'Invalid Username and Password'})
+        else:
+            return render(request, 'orders/login.html', {'error message': 'Invalid Username and Password'})
+    return render(request, 'orders/login.html')
+
 
 # view customer profiles based on ids
-def customerProfile(request, pk = None):
-	if pk:
-		user = User.objects.get(pk = pk)
-	else:
-		user = request.user
-	return render(request, 'orders/profile.html', {'user':user})
+def customerProfile(request, pk=None):
+    if pk:
+        user = Customer.objects.get(pk=pk)
+    else:
+        user = request.user
+    return render(request, 'orders/profile.html', {'user': user})
+
+
+# create the customer profiles
+def createCustomer(request):
+    form = CustomerForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return render(request, 'orders/profile_form.html', context={'form': form, 'title': "Complete Profile"})
+
+
+# update any details on customer profiles
+def updateCustomer(request):
+    form = CustomerForm(request.POST or None, instance=request.user.customer)
+    if form.is_valid():
+        form.save()
+        return redirect('profile')
+    return render(request, 'orders/profile_form.html', context={'form': form, 'title': "Update Profile"})
